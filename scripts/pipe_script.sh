81604cb158ee6de31af297d31bda49684ea450c3
Fran-bot0 <francisco.narciso999@gmail.com>
2024-06-23 10:38:35 +0100

    Update pipe_script.sh

--- scripts/pipe_script.sh
+++ scripts/pipe_script.sh
@@ -3,20 +3,20 @@
 # Exporting bin path for the symbolic links to work
 export PATH="$HOME/pipeline_project/bin:$PATH"
 
-# Asking the user whihc organism the pipeline will run with and making the necessary directories if none exist
+# Asking the user which organism the pipeline will run with.
 read -p  'Please insert of which organism to download the primary_assembly (ex: Homo sapiens): ' input_organism
 
+# Making the input_organism lowercase and separated by an underscore.
 organism=$(tr -s ' ' '_' <<< ${input_organism,,})
 
+# Creating the necesary directories if none exist.
 mkdir -p /home/francisco/pipeline_project/pipeline_data/$organism/input_files
 mkdir -p /home/francisco/pipeline_project/pipeline_data/$organism/output_files
 cd /home/francisco/pipeline_project/pipeline_data/$organism/input_files
 
-
-# Downloads the primary assembly of the specified organism from Ensembl. If the primary assembly doesn't exist, it downloads the toplevel file
-# Also doenloads the gtf and the vcf files
+# Downloads the primary assembly of the specified organism from Ensembl. If the primary assembly doesn't exist, it 
+# downloads the toplevel file. Also downloads the gtf and the vcf files.
 python /home/francisco/pipeline_project/scripts/downloader.py $organism || exit
-
 
 # Extracting gtf file
 yes n | gunzip -k gtf_$organism.gtf.gz
@@ -33,7 +33,6 @@ yes n | gunzip -k primary_assembly_$organism.fa.gz
 # Indexing the genome using samtools
 samtools faidx primary_assembly_toindex_$organism.fa.gz
 
-
 # Indexing the genome/transcriptome using STAR
 [[ -d STAR ]] || STAR --runThreadN 2 \
 		      --runMode genomeGenerate \
@@ -44,14 +43,12 @@ samtools faidx primary_assembly_toindex_$organism.fa.gz
      	 	      --genomeSAindexNbases 10 \
 		      --limitBAMsortRAM 1362848454
 
-
 # Indexing the vcf and sorted gtf files using tabix
 tabix vcf_$organism.vcf.gz
 tabix gtf_$organism.sorted.gtf.gz
 
 # Creating a sequence dictionary using picard
 java -Xmx4g -jar /home/francisco/pipeline_project/software/picard/picard.jar CreateSequenceDictionary -R primary_assembly_toindex_$organism.fa.gz
-
 
 cd /home/francisco/pipeline_project/pipeline_data/$organism/output_files
 
